f410ffc5266123fe9219f06ce808de83d184e54f
awalakaushik <kawala@uh.edu>
2020-07-11 01:23:33 -0500

    Add note service

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,30 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+    private final NoteMapper noteMapper;
+
+    public NoteService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    public int addNote(Note note) {
+        return noteMapper.addNote(new Note(null, note.getNoteTitle(), note.getNoteDescription(), note.getUserId()));
+    }
+
+    public Note getNote(Integer noteId) {
+        return noteMapper.getNote(noteId);
+    }
+
+    public Note[] getNotes(String username) {
+        return noteMapper.getNotes(username);
+    }
+
+    public void deleteNote(Integer noteId) {
+        noteMapper.deleteNote(noteId);
+    }
+}
