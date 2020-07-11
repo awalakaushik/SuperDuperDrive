6d67365dd62103a904a10fd78db14e9644af48c9
awalakaushik <kawala@uh.edu>
2020-07-11 12:05:22 -0500

    Add note controller and separate its methods

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -0,0 +1,94 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home/note")
+public class NoteController {
+
+    private NoteService noteService;
+    private UserService userService;
+
+    public NoteController(NoteService noteService, UserService userService) {
+        this.noteService = noteService;
+        this.userService = userService;
+    }
+
+    @GetMapping(value = {"/notes"})
+    public String getNotes(Authentication authentication, Model model) {
+
+        model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+        return "home";
+    }
+
+    @PostMapping(value = {"/note/add"})
+    public String addNote(Authentication authentication, Note note, Model model) {
+
+        String errorMessage;
+
+        note.setUserId(userService.getUser(authentication.getName()).getUserid());
+
+        int rowsAdded = noteService.addNote(note);
+
+        if (rowsAdded < 0) {
+            errorMessage = "There was an error adding the note. Please try again!";
+
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+            return "result";
+        }
+
+        model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+        note.setNoteTitle("");
+        note.setNoteDescription("");
+
+        return "result";
+    }
+
+    @PostMapping(value = {"/note/edit"})
+    public String editNote(Authentication authentication, Note note, Model model) {
+
+        String errorMessage;
+
+        int rowsUpdated = noteService.updateNote(note);
+
+        if (rowsUpdated < 0) {
+            errorMessage = "There was an error updating the note. Please try again!";
+
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+            return "result";
+        }
+
+        model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+        note.setNoteTitle("");
+        note.setNoteDescription("");
+
+        return "result";
+    }
+
+    @DeleteMapping(value = {"/note/delete"})
+    public String deleteNote(Authentication authentication, Note note, Model model) {
+
+        noteService.deleteNote(note.getNoteId());
+
+        model.addAttribute("notes", noteService.getNotes(authentication.getName()));
+
+        return "result";
+    }
+
+}
