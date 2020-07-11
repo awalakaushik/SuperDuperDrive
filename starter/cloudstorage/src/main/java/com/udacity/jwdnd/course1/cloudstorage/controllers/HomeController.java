afe6ca5106715ffa55f4d02efa6603d006814340
awalakaushik <kawala@uh.edu>
2020-07-11 01:24:01 -0500

    Add get mapping route for fetching notes

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+    private NoteService noteService;
+
+    public HomeController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
     @GetMapping()
     public String getHomePage() {
         return "home";
     }
+
+    @GetMapping("/notes")
+    public String getNotes(Authentication authentication, Note note, Model model) {
+        model.addAttribute("notes", this.noteService.getNotes(authentication.getName()));
+        return "home";
+    }
+
 }
