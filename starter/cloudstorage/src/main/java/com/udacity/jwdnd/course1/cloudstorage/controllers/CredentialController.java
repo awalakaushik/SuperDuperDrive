5a02511539c16e9d6c412a59c5a9cd8fe59a3a2c
awalakaushik <kawala@uh.edu>
2020-07-11 23:40:24 -0500

    Add CredentialController

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialController.java
@@ -0,0 +1,56 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.HashService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CredentialController {
+
+    private CredentialService credentialService;
+    private UserService userService;
+    private HashService hashService;
+
+    public CredentialController(CredentialService credentialService, UserService userService, HashService hashService) {
+        this.credentialService = credentialService;
+        this.userService = userService;
+        this.hashService = hashService;
+    }
+
+    @PostMapping(value = {"/credentials"})
+    public String addOrUpdateCredentials(Authentication authentication, Credential credentials, Model model) {
+
+        String errorMessage;
+        int rowsAffected;
+
+        if (credentials.getCredentialid() != null) {
+            rowsAffected = credentialService.updateCredentials(credentials);
+        } else {
+            credentials.setUserid(userService.getUser(authentication.getName()).getUserid());
+            rowsAffected = credentialService.addCredential(credentials);
+        }
+
+        if (rowsAffected <= 0) {
+            errorMessage = "There was an error adding/updating the credentials. Please try again!";
+            model.addAttribute("errorMessage", errorMessage);
+            return "result";
+        }
+
+        return "result";
+    }
+
+    @GetMapping("/credentials/delete/{credentialid}")
+    public String deleteCredentials(@PathVariable Integer credentialid) {
+        credentialService.deleteCredential(credentialid);
+        return "result";
+    }
+
+}
