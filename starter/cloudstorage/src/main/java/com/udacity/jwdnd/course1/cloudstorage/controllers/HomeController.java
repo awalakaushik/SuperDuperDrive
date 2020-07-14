26ec4a380c173cd300e657dcdde3ad35da2d6bd0
awalakaushik <kawala@uh.edu>
2020-07-13 19:56:29 -0500

    wire UI

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.security.core.Authentication;
@@ -18,15 +20,20 @@ import java.util.List;
 public class HomeController {
 
     private NoteService noteService;
+    private CredentialService credentialService;
 
-    public HomeController(NoteService noteService) {
+    public HomeController(NoteService noteService, CredentialService credentialService) {
         this.noteService = noteService;
+        this.credentialService = credentialService;
     }
 
     @GetMapping()
     public String getHomePage(Authentication authentication, Model model) {
         List<Note> notes = noteService.getNotes(authentication.getName());
+        List<Credential> credentials = credentialService.getCredentials(authentication.getName());
+
         model.addAttribute("notes", notes);
+        model.addAttribute("credentials", credentials);
         return "home";
     }
 
